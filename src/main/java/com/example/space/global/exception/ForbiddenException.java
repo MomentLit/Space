580763091e6b9ff2a47e45c283de0590022b326b
주환 <jhlarry1109@gmail.com>
2026-06-18 15:15:22 +0900

    feat: custom exception 추가

---
+++ src/main/java/com/example/space/global/exception/ForbiddenException.java
@@ -0,0 +1,7 @@
+package com.example.space.global.exception;
+
+public class ForbiddenException extends SpaceException {
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
